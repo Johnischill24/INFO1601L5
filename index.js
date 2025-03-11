be66c12cf3a8ad864a8cdf6647de62036d5eed37
Craig.Williams <craigjmwilliams@gmail.com>
2025-03-11 13:26:48 -0400

    Made more changes

--- index.js
+++ index.js
@@ -178,6 +178,64 @@ function printDate(){
     console.log(new Date().toLocaleTimeString());
  }
        
- setInterval(printDate, 1000);
+ //setInterval(printDate, 1000);
+
+ setInterval(printDate, 2000);
 */
+
+
+/*
+//ARRAYS - collection of elements
+
+let arr = []//empty array
+let arr2 = [1, 2, 3, 4, 5, 6, 7]//initialize with elements
+let arr3 = [1, 12.9, "Hi", '🎈', ]// heterogeneous array
+
+console.log(arr2[0]);// 1
+console.log(arr3[3]);//🎈
+*/
+
+
+//USING A For Loop in an Array 
+
+/*
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+
+for(let i=0; i< arr.length; i++){
+  console.log(arr[i])
+}
+
+for (let item of arr){// for of loop
+   console.log(item);
+}
+*/
+
+/*
+
+//ARRAY METHODS
+let arr = [ -5, 16, 33, 42, 103, 344];
+
+console.log(arr.includes(-5));//true
+
+arr.push(11);//adds item to the end
+
+console.log(arr)//[ -5, 16, 33, 42, 103, 344, 11];
+
+let lastItem = arr.pop();//removes last item
+
+console.log(lastItem, arr);//11, [ -5, 16, 33, 42, 103, 344]
+
+arr.unshift(22);//adds item to the front
+
+console.log(arr);//[22, -5, 16, 33, 42, 103, 344]
+
+let firstItem = arr.shift();//removes first item
+console.log(firstItem, arr);//22, [-5, 16, 33, 42, 103, 344]
+
+let reversed = arr.reverse();//creates a new array in reverse order 
+console.log(reversed);//[344, 103, 42, 33, 16, -5]
+console.log(arr.join('-'));//"-5-103-16-33-344-42" joins array with provided separator
+*/
+
+
 
