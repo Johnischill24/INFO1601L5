fdfb4c8e70df1877c0b11442fd7ffd6d84e5608b
Craig.Williams <craigjmwilliams@gmail.com>
2025-03-11 12:29:33 -0400

    Made changes to Lab 5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let name = "bob";
+/*let name = "bob";
 let age = 24;
 
 console.log(typeof(name))//string
@@ -9,3 +9,13 @@ console.log(`Hello my name is ${name}, I'm ${age} years old`);
 
 console.log(`I was born in ${2020 - age}`)
 //I was born in 1966
+*/
+
+//Task 4.1
+console.log('1' == 1); //true
+console.log(1 == true); //true
+console.log("false" == false); // "false" is not false
+console.log("false" == true); // "false" is not true either
+
+if("false")
+   console.log("Hello false!") //but "false" is true enough here
